fafd474b10cf5f67ef17bb3968249f4d854ad41b
Antonio Murdaca <runcom@redhat.com>
2016-04-18 13:24:07 +0200

    Fix audit, dockerhooks and distros patches cross compile and CI
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>

--- daemon/dockerhooks/dockerhooks_windows.go
+++ daemon/dockerhooks/dockerhooks_windows.go
@@ -1,12 +1,10 @@
 package dockerhooks
 
-import (
-	"github.com/opencontainers/specs/specs-go"
-)
+import "github.com/opencontainers/specs/specs-go"
 
 // Generate docker hooks Prestart and poststart specs
 func Generate(specHooks specs.Hooks, configPath string) specs.Hooks {
-	return nil
+	return specHooks
 }
 
 // TotalHooks returns the number of hooks to be used
