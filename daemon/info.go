a363a633a402116e8fc263a36762f3cc263527c8
Antonio Murdaca <runcom@redhat.com>
2017-03-23 10:07:32 +0100

    Set PkgVersion at rpmbuild time. Default: "<unknown>"
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>

--- daemon/info.go
+++ daemon/info.go
@@ -3,7 +3,6 @@ package daemon
 import (
 	"fmt"
 	"os"
-	"os/exec"
 	"runtime"
 	"sync/atomic"
 	"time"
@@ -17,7 +16,6 @@ import (
 	"github.com/docker/docker/pkg/parsers/kernel"
 	"github.com/docker/docker/pkg/parsers/operatingsystem"
 	"github.com/docker/docker/pkg/platform"
-	"github.com/docker/docker/pkg/rpm"
 	"github.com/docker/docker/pkg/sysinfo"
 	"github.com/docker/docker/pkg/system"
 	"github.com/docker/docker/registry"
@@ -59,14 +57,6 @@ func (daemon *Daemon) SystemInfo() (*types.Info, error) {
 	}
 
 	sysInfo := sysinfo.New(true)
-	dockerPath, err := exec.LookPath("docker")
-	if err != nil {
-		logrus.Warnf("could not look docker binary path: %v", err)
-	}
-	packageVersion, err := rpm.Version(dockerPath)
-	if err != nil {
-		logrus.Warnf("could not retrieve docker rpm version: %v", err)
-	}
 
 	var cRunning, cPaused, cStopped int32
 	daemon.containers.ApplyAll(func(c *container.Container) {
@@ -139,7 +129,7 @@ func (daemon *Daemon) SystemInfo() (*types.Info, error) {
 		LiveRestoreEnabled: daemon.configStore.LiveRestoreEnabled,
 		SecurityOptions:    securityOptions,
 		Isolation:          daemon.defaultIsolation,
-		PkgVersion:         packageVersion,
+		PkgVersion:         "<unknown>",
 	}
 
 	// Retrieve platform specific info
@@ -158,7 +148,6 @@ func (daemon *Daemon) SystemInfo() (*types.Info, error) {
 
 // SystemVersion returns version information about the daemon.
 func (daemon *Daemon) SystemVersion() types.Version {
-	pkgVersion, _ := rpm.Version("/usr/bin/docker")
 	v := types.Version{
 		Version:       dockerversion.Version,
 		GitCommit:     dockerversion.GitCommit,
@@ -168,7 +157,7 @@ func (daemon *Daemon) SystemVersion() types.Version {
 		Arch:          runtime.GOARCH,
 		BuildTime:     dockerversion.BuildTime,
 		Experimental:  daemon.configStore.Experimental,
-		PkgVersion:    pkgVersion,
+		PkgVersion:    "<unknown>",
 	}
 
 	kernelVersion := "<unknown>"
