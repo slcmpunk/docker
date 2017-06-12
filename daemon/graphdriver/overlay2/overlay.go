0a5a47644d593ff34d26c690c2f97131f979df1a
Antonio Murdaca <runcom@redhat.com>
2017-06-13 01:16:56 +0200

    overlay2: fix compilation
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>

--- daemon/graphdriver/overlay2/overlay.go
+++ daemon/graphdriver/overlay2/overlay.go
@@ -10,18 +10,19 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"strconv"
 	"strings"
 	"syscall"
 
 	"github.com/Sirupsen/logrus"
-
 	"github.com/docker/docker/daemon/graphdriver"
 	"github.com/docker/docker/pkg/archive"
 	"github.com/docker/docker/pkg/chrootarchive"
 	"github.com/docker/docker/pkg/directory"
 	"github.com/docker/docker/pkg/idtools"
 	"github.com/docker/docker/pkg/mount"
-
+	"github.com/docker/docker/pkg/parsers"
+	"github.com/docker/docker/pkg/parsers/kernel"
 	"github.com/opencontainers/runc/libcontainer/label"
 )
 
@@ -91,6 +92,10 @@ func init() {
 // If overlay filesystem is not supported on the host, graphdriver.ErrNotSupported is returned as error.
 // If a overlay filesystem is not supported over a existing filesystem then error graphdriver.ErrIncompatibleFS is returned.
 func Init(home string, options []string, uidMaps, gidMaps []idtools.IDMap) (graphdriver.Driver, error) {
+	opts, err := parseOptions(options)
+	if err != nil {
+		return nil, err
+	}
 	if err := supportsOverlay(); err != nil {
 		return nil, graphdriver.ErrNotSupported
 	}
@@ -101,6 +106,11 @@ func Init(home string, options []string, uidMaps, gidMaps []idtools.IDMap) (grap
 	}
 	if fsName, ok := graphdriver.FsNames[fsMagic]; ok {
 		backingFs = fsName
+	}
+
+	v, err := kernel.GetKernelVersion()
+	if err != nil {
+		return nil, err
 	}
 
 	// check if they are running over btrfs, aufs, zfs, overlay, or ecryptfs
@@ -145,6 +155,27 @@ func Init(home string, options []string, uidMaps, gidMaps []idtools.IDMap) (grap
 
 type overlayOptions struct {
 	overrideKernelCheck bool
+}
+
+func parseOptions(options []string) (*overlayOptions, error) {
+	o := &overlayOptions{}
+	for _, option := range options {
+		key, val, err := parsers.ParseKeyValueOpt(option)
+		if err != nil {
+			return nil, err
+		}
+		key = strings.ToLower(key)
+		switch key {
+		case "overlay2.override_kernel_check":
+			o.overrideKernelCheck, err = strconv.ParseBool(val)
+			if err != nil {
+				return nil, err
+			}
+		default:
+			return nil, fmt.Errorf("overlay2: Unknown option %s\n", key)
+		}
+	}
+	return o, nil
 }
 
 func supportsOverlay() error {
