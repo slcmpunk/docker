6f56618838997d102202ea54d7dc791a0730fb1e
Antonio Murdaca <runcom@redhat.com>
2017-11-21 11:16:13 +0100

    BACKPORT: Fix the data model inconsistency that breaks daemon upgrade to 1.14-dev
    Upstream reference: https://github.com/docker/libnetwork/pull/1620
    Fix https://bugzilla.redhat.com/show_bug.cgi?id=1464790
    
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>

--- vendor/src/github.com/docker/libnetwork/sandbox_dns_unix.go
+++ vendor/src/github.com/docker/libnetwork/sandbox_dns_unix.go
@@ -168,8 +168,8 @@ func (sb *sandbox) setExternalResolvers(content []byte, addrType int, checkLoopb
 			hostLoopback = dns.IsIPv4Localhost(ip)
 		}
 		sb.extDNS = append(sb.extDNS, extDNSEntry{
-			ipStr:        ip,
-			hostLoopback: hostLoopback,
+			IPStr:        ip,
+			HostLoopback: hostLoopback,
 		})
 	}
 }
