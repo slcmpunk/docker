1a0e30838b25d9857368cd90a959aae12ec9196b
Giuseppe Scrivano <gscrivan@redhat.com>
2018-03-11 21:35:44 +0100

    volume: allow a prefix for all bind mounts
    Introduce a new config --bind-mount-prefix=PREFIX that adds a prefix
    to the source for every bind mount.  This allows Docker to run inside
    of a container, and be able to see the host rootfs.
    
    As an example: running Docker with --bind-mount-prefix=/host, will
    convert:
    
    docker run -v /var/lib/mariadb:/var/lib/mariadb ...
    
    to:
    
    docker run -v /host/var/lib/mariadb:/var/lib/mariadb ...
    
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>
    (cherry picked from commit 38e1b7def8b643d109a53a0d4854db6ea91a78df)

--- daemon/config_unix.go
+++ daemon/config_unix.go
@@ -37,6 +37,7 @@ type Config struct {
 	InitPath             string                   `json:"init-path,omitempty"`
 	SeccompProfile       string                   `json:"seccomp-profile,omitempty"`
 	EnableSecrets        bool                     `json:"enable-secrets"`
+	BindMountPrefix      string                   `json:"bind-mount-prefix"`
 }
 
 // bridgeConfig stores all the bridge driver specific
@@ -91,6 +92,7 @@ func (config *Config) InstallFlags(flags *pflag.FlagSet) {
 	flags.Int64Var(&config.CPURealtimeRuntime, "cpu-rt-runtime", 0, "Limit the CPU real-time runtime in microseconds")
 	flags.StringVar(&config.SeccompProfile, "seccomp-profile", "", "Path to seccomp profile")
 	flags.BoolVar(&config.EnableSecrets, "enable-secrets", true, "Enable Secrets")
+	flags.StringVar(&config.BindMountPrefix, "bind-mount-prefix", "", "Specify a prefix to prepend to the source of a bind mount")
 
 	config.attachExperimentalFlags(flags)
 }
